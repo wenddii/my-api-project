73a623efb4127a726034b7e8fb2db3cd76c69232
wenddii <wendwosen03@gmail.com>
2026-04-08 12:21:41 +0300

    Delete Blog directory

--- Blog/Api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import BlogPostViewSet,CommentViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'posts',BlogPostViewSet,basename='post')
