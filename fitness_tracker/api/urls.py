747dd75b690ec6122907c62612f07eb0e86a85df
wenddii <wendwosen03@gmail.com>
2026-04-10 09:52:04 +0300

    Delete fitness_tracker directory

--- fitness_tracker/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from .views import (
-    WorkoutListCreateView, WorkoutDetailView)
-
-urlpatterns = [
-    path('workouts/', WorkoutListCreateView.as_view()),            # GET list, POST create
-    path('workouts/<int:pk>/', WorkoutDetailView.as_view()),       # GET, PUT/PATCH, DELETE single
-    path('api-auth/', include('rest_framework.urls')),
-]
