868294b40c92023bd0f8ad70794edd106d2f6d06
wenddii <wendwosen03@gmail.com>
2026-04-10 09:55:08 +0300

    Delete Blog_api directory

--- Blog_api/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PostListCreateView, PostDetailView, CommentCreateView
-
-urlpatterns = [
-    path('posts/', PostListCreateView.as_view()),
-    path('posts/<int:pk>/', PostDetailView.as_view()),
-    path('posts/<int:post_id>/comments/', CommentCreateView.as_view()),
-]
