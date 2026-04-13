2299567187780282de4cb10bd168703ae40a7ecf
wenddii <wendwosen03@gmail.com>
2026-04-13 15:16:48 +0300

    tried another models to practice nested serializrs

--- Blog/blog_app/models.py
+++ Blog/blog_app/models.py
@@ -2,22 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=100)
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
 
 
-class Post(models.Model):
+class Article(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
-
-
-class Comment(models.Model):
     content = models.TextField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles')
+    tags = models.ManyToManyField(Tag, related_name='articles')
 
 
-class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+class Review(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.IntegerField()
+    comment = models.TextField()
+
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookmarks')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='bookmarks')
