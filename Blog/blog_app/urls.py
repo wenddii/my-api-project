140d9387d5135dfe52deb2f438a5927c2c94a4d7
wenddii <wendwosen03@gmail.com>
2026-04-15 12:28:26 +0300

    added JWT authentication and tested it with postman

--- Blog/blog_app/urls.py
+++ Blog/blog_app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from .views import PostListCreateView,PostDetailView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
+    path('posts/', PostListCreateView.as_view()),
+    path('posts/<int:pk>/', PostDetailView.as_view()),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
+
