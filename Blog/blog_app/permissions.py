abb1e6067f85121941e0ab28271b9fdd8e9844fc
wenddii <wendwosen03@gmail.com>
2026-04-26 13:30:41 +0300

    made changes to the folder

--- Blog/blog_app/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import BasePermission 
-
-class IsAutherOrReadOnly(BasePermission):
-    def has_object_permission(self,request,view,obj):
-        if request.method in ['GET','HEAD','OPTIONS']:
-            return True
-        return obj.author == request.user
-    
